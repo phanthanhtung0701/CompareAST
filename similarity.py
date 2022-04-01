c43cbd08870213991e2f19f5a56d7c811a1096ee
phanthanhtung0701 <phantung98nd@gmail.com>
2022-04-01 15:51:59 +0700

    Change LCS function and Add TF-IDF similarity

--- similarity.py
+++ similarity.py
@@ -1,52 +1,52 @@
-# LCS
-def lcs_position(seq1, seq2):
-    """
-    Obtain the position information of longest common subsequence.
-    Args:
-        seq1: A sequence to be iterated.
-        seq2: A sequence to be iterated.
-    Returns:
-        The position information of longest common subsequence.
-    """
-    m, n = len(seq1), len(seq2)
-    # initialize
-    dp = [[[] for _ in range(n + 1)] for _ in range(m + 1)]
-    # fill
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            # top first
-            if seq1[m - i] == seq2[n - j]:
-                dp[i][j] = dp[i - 1][j - 1] + [(m - i, n - j)]
+# LCS equal to the Levenshtein length
+def lcs(X, Y):
+    '''
+        The function returns the length of the longest common subsequence
+        of two sequences X and Y.
+        @param X - list of tokens of the first program
+        @param Y - list of tokens of the second program
+    '''
+    m = len(X)
+    n = len(Y)
+
+    if m == 0 or n == 0:
+        return 0
+
+    # m + 1 rows
+    # n + 1 columns
+    L = [[0] * (n + 1) for i in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]:
+                L[i][j] = L[i-1][j-1] + 1
             else:
-                # column preference
-                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j], key=len)
-    return dp[-1][-1][::-1]
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+
+    return L[m][n]
 
 
-def normalized_dist(seq1, seq2):
+def lcs_based_coeff(subseq1, subseq2):
+    """The function returns coefficient based on the length
+    of the longest common subsequence.
+    This coefficient describes how same two sequences.
+    @param subseq1 - the first sequence
+    @param subseq2 - the second sequnce
     """
-    Obtain the normalized distance between two sequences.
-    Args:
-        seq1: A sequence to be iterated.
-        seq2: A sequence to be iterated.
-    Returns:
-        The normalized distance between two sequences.
-    """
-    m, n = len(seq1), len(seq2)
-    # initialize
-    dp = [[i + j for j in range(n + 1)] for i in range(m + 1)]
-    # fill
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            dist = 1 if seq1[i-1] != seq2[j-1] else 0
-            dp[i][j] = min(dp[i-1][j-1] + dist,
-                           min(dp[i-1][j], dp[i][j-1]) + 1)
-    return dp[-1][-1] / max(m, n)
+
+    count_elem1 = len(subseq1)
+    count_elem2 = len(subseq2)
+
+    if (count_elem1 * count_elem2) == 0:
+        return 0.0
+
+    return (2 * lcs(subseq1, subseq2)) / (count_elem1 + count_elem2)
 
 
 # Sequence Matcher
 from difflib import SequenceMatcher
-
 
 # Damerau–Levenshtein distance
 import editdistance
@@ -57,3 +57,36 @@ def compare_stats(seq1, seq2):
     avg_len = (len(seq1) + len(seq2)) / 2.0
     percent = 1 - (dld / avg_len)
     return percent, dld
+
+
+# TF-IDF
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def identityFunction(file):
+    return file
+
+
+def tf_idf_similarity(seq1, seq2):
+    docs = [seq1, seq2]
+
+    VOCAB_LIMIT = 2000  # Can be increased if efficency is not an issue
+    vectorizer = TfidfVectorizer(
+        analyzer='word',
+        tokenizer=identityFunction,
+        preprocessor=identityFunction,
+        # Consider unigrams and bigrams only
+        ngram_range=(1, 1),
+        sublinear_tf=True,  # (1+log(tf)) instead of just tf
+        max_features=VOCAB_LIMIT,
+        encoding="utf-8",
+        decode_error="ignore",
+        stop_words=None,
+        lowercase=False,
+        norm="l2"  # Each row will be unit normalized
+    )
+    S = vectorizer.fit_transform(docs)
+
+    tfm = linear_kernel(S, S)
+    return tfm
