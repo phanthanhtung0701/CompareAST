60ef087c53654236da81f977292db77921a42f94
Tùng Phan <phantung98nd@gmail.com>
2022-04-24 23:40:40 +0700

    Add file compare two source code

--- compare_two_code.py
+++ compare_two_code.py
@@ -8,7 +8,7 @@ from similarity import *
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i_1', '--input_1', type=str, default='test_data/test_11.cpp', help='path to directory')
+    parser.add_argument('-i_1', '--input_1', type=str, default='test_data/test_1.cpp', help='path to directory')
     parser.add_argument('-i_2', '--input_2', type=str, default='test_data/test_2.cpp', help='path to directory')
     parser.add_argument('-c', '--compare', type=str, default='AST_CC', help='algorithm used in comparison ast')
 
